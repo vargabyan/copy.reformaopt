6288abb51fe3bd0c5bce05e6042df4071a3a40d0
vargabyan <vardan.gabrielyan.it@gmail.com>
2023-08-24 10:39:18 +0500

    #home-page-1

---
+++ home/index.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="../fonts.css">
+<link rel="stylesheet" href="./home.css">
+
+<div class="container-width">
+
+    <div class="about-company">
+        <div class="about-company_top">
+            <p class="big-text">Производственная компания</p>
+            <p class="small-text">по печати и пошиву одежды</p>
+        </div>
+        <div class="about-company_bottom">
+            <div class="about-company_bottom_image-box">
+                <div class="wholesale-order">
+                    <div class="background-orange">
+                        Оптовый <br> заказ
+                    </div>
+                </div>
+                <img class="wholesale-Order-img" src="./images/DSC02748-fin%202.png" alt="">
+            </div>
+            <div class="about-company_bottom_video-box">
+                <img class="video" src="images/image 32.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="we-work">
+        Работаем с <span>2009</span> года
+    </div>
+
+    <div class="production">
+        <div class="production_slider-box">
+
+        </div>
+        <div class="production_title">
+            Собственное
+            <span>швейное и печатное </span>
+            производство
+        </div>
+    </div>
+
+</div>
