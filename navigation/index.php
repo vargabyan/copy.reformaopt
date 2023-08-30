c94d1ee3027b97f1c62e71ff55050d06efe80bec
vargabyan <vardan.gabrielyan.it@gmail.com>
2023-08-30 14:07:25 +0500

    #navigation-1

---
+++ navigation/index.php
@@ -0,0 +1,52 @@
+<link rel="stylesheet" href="/fonts.css">
+<link rel="stylesheet" href="/navigation/navigation.css">
+
+<div class="container-width">
+    <div class="navBar">
+        <ul class="navbar_wrapper-items">
+            <li class="navbar_item navbar_wrapper-logo">
+                <img class="navbar_logo" src="/navigation/images/RE.png" alt="">
+                Печать и пошив <br> одежды оптом
+            </li>
+            <li class="navbar_item button-toggle">
+                <a class="item_link link-toggle" href="">Печать</a>
+                <div class="navbar_dropdown">
+                    <a class="dropdown_link" href="">Футболки</a>
+                    <a class="dropdown_link" href="">Толстовки</a>
+                    <a class="dropdown_link" href="">Бейсболки</a>
+                    <a class="dropdown_link" href="">Свитшоты</a>
+                    <a class="dropdown_link" href="">Жилеты</a>
+                    <a class="dropdown_link" href="">Рубашки</a>
+                    <a class="dropdown_link" href="">Ветровки</a>
+                </div>
+            </li>
+            <li class="navbar_item button-toggle">
+                <a class="item_link link-toggle" href="">Пошив одежды</a>
+                <div class="navbar_dropdown">
+                    <a class="dropdown_link" href="">test</a>
+                </div>
+            </li>
+            <li class="navbar_item button-toggle">
+                <a class="item_link link-toggle" href="">Технологии</a>
+                <div class="navbar_dropdown">
+                    <a class="dropdown_link" href="">test</a>
+                </div>
+            </li>
+            <li class="navbar_item">
+                <a class="item_link" href="">Отзывы</a>
+            </li>
+            <li class="navbar_item">
+                <a class="item_link" href="">Контакты</a>
+            </li>
+            <li class="navbar_item">
+                <a class="item_link link-orange" href="mailto:reformaopt@yandex.ru">reformaopt@yandex.ru</a>
+            </li>
+            <li class="navbar_item">
+                <a class="item_link link-orange" href="tel:+73433020700">+7 (343) 302-07-00</a>
+            </li>
+        </ul>
+        <a class="order-call" href="">
+            Заказать звонок
+        </a>
+    </div>
+</div>
